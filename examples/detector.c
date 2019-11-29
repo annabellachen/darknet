9eb8d27edb7c2111d44227e1add4b985610dcf85
annabellachen <biying.chen@u.nus.edu>
2019-11-29 11:08:27 +0800

    trial 10

--- examples/detector.c
+++ examples/detector.c
@@ -1,7 +1,7 @@
 #include "darknet.h"
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
-void validate_detector_recall_internal(network net)
+void validate_detector_recall_internal(network *net)
 {
     set_batch_network(net, 1);
     fprintf(stderr, "Learning Rate: %g, Momentum: %g, Decay: %g\n", net->learning_rate, net->momentum, net->decay);
@@ -220,7 +220,7 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
         //strcat( dest, result);
         //strcat( dest, ".weights");
         //validate_detector_recall(cfgfile,dest);
-        validate_detector_recall_internal(nets)
+        validate_detector_recall_internal(*net)
     }
 #ifdef GPU
     if(ngpus != 1) sync_nets(nets, ngpus, 0);
