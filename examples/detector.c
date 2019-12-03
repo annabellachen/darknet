56c1f8c40305da295259afae5b6b8c2737e6094a
annabellachen <biying.chen@u.nus.edu>
2019-12-03 22:42:42 +0800

    trial 50

--- examples/detector.c
+++ examples/detector.c
@@ -1,7 +1,7 @@
 #include "darknet.h"
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
-void validate_detector_recall_internal(network *net,int iteration)
+float validate_detector_recall_internal(network *net,int iteration)
 {
     set_batch_network(net, 1);
     fprintf(stderr, "Learning Rate: %g, Momentum: %g, Decay: %g\n", net->learning_rate, net->momentum, net->decay);
@@ -75,14 +75,15 @@ void validate_detector_recall_internal(network *net,int iteration)
         free_image(orig);
         free_image(sized);
     }
-    FILE * fp;
-    fp = fopen ("//content/darknet/examples/validationResult.txt","ab+");
+    //FILE * fp;
+    //fp = fopen ("//content/darknet/examples/validationResult.txt","ab+");
     /* write 10 lines of text into the file stream*/
-    fprintf (fp, "Epoch: %5d\tIOU: %.2f%%\tRecall:%.2f%%\n", iteration, sum_iou/m, sum_recall/m); 
+    //fprintf (fp, "Epoch: %5d\tIOU: %.2f%%\tRecall:%.2f%%\n", iteration, sum_iou/m, sum_recall/m); 
     /* close the file*/  
-    fclose (fp);
+    //fclose (fp);
     //fprintf(stderr, "Epoch: %5d\tIOU: %.2f%%\tRecall:%.2f%%\n", iteration, sum_iou/m, sum_recall/m);
-    free(fp);
+    //free(fp);
+    return sum_iou/m;
 }
 
 void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, int ngpus, int clear)
@@ -209,7 +210,8 @@ void train_detector(char *datacfg, char *cfgfile, char *weightfile, int *gpus, i
 
         i = get_current_batch(net);
         printf("%ld: %f, %f avg, %f rate, %lf seconds, %d images\n", get_current_batch(net), loss, avg_loss, get_current_rate(net), what_time_is_it_now()-time, i*imgs);
-        //validate_detector_recall_internal(net,get_current_batch(net));
+        float testresult=validate_detector_recall_internal(net,get_current_batch(net));
+        printf(" %f\n",testresult)
         if(i%5==0){
 #ifdef GPU
             if(ngpus != 1) sync_nets(nets, ngpus, 0);
